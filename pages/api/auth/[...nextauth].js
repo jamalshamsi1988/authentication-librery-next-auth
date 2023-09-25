7baf62fcb16b7cb189cd6c962f5f28fafb9ac881
jamal.shamsi <jamal.shamsi1988@gmail.com>
2023-09-25 23:11:01 +0330

    signin page created

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -6,21 +6,21 @@ const authOptions = {
     providers : [
         CredentialProvider({
             name : "credentials" ,
-            Credential : {
-                email : {
-                    label : "Email",
-                    type : "text" ,
-                    placeholder : " Enter your email",
-                },
-                password :{
-                    label : "Password" ,
-                    type : "password"
-                },
-                async authorize(credentials , req){
-                    return { name : "jamal"}
-                }
-            },
+            // Credential : {
+            //     email : {
+            //         label : "Email",
+            //         type : "text" ,
+            //         placeholder : " Enter your email",
+            //     },
+            //     password :{
+            //         label : "Password" ,
+            //         type : "password"
+            //     },
+            // },
             
+            async authorize(credentials , req){
+                return { name : "jamal"}
+            }
         })
     ]
 };
--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,17 @@
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
+// import  {signIn}  from 'next-auth/react';
+
+
 export default function Home() {
   return (
     <div className={styles.container}>
       <h1>Next-Auth Cridentials</h1>
       <button><Link href="/signup">Sign Up || Register</Link></button>
+      <Link href="/signin">
+      <button >Login</button>
+      </Link>
     </div>
   )
 }
