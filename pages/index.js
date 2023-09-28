f1e0c370154f970461270e637abaf7486ac84ed3
jamal.shamsi <jamal.shamsi1988@gmail.com>
2023-09-28 16:42:34 +0330

    show the component if user authenticated or not with useSession

--- pages/index.js
+++ pages/index.js
@@ -2,32 +2,43 @@
 
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
-import { signOut } from 'next-auth/react'
+import { signOut, useSession } from 'next-auth/react'
 
 // import  {signIn}  from 'next-auth/react';
 
 
 export default function Home() {
 
+  const {status}=useSession();
 
   const logOutHandler= ()=>{
     signOut();
   }
-  
+
   return (
     <div className={styles.container}>
       <h1>Next-Auth Credentials</h1>
-      <Link href="/dashbord">
+     {
+      status === "authenticated" ?
+       <>
+       <Link href="/dashbord">
       <button >Dashbord</button>
       </Link>
-      
-      <button><Link href="/signup">Sign Up || Register</Link></button>
-
-      <Link href="/signin">
-      <button >Login</button>
-      </Link>
-
       <button onClick={logOutHandler}>Log Out</button>
+      
+      
+      </> : null
+     }
+     
+     {
+      status === "unauthenticated" ?
+       <>
+        <button><Link href="/signup">Sign Up || Register</Link></button>
+          <Link href="/signin">
+          <button >Login</button>
+          </Link>
+        </> : null
+     } 
     </div>
   )
 }
