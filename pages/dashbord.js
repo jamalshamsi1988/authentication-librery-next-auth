480973a2abc7544919be88be57ee3e942c0d94ab
jamal.shamsi <jamal.shamsi1988@gmail.com>
2023-09-28 17:20:35 +0330

    completed update info UI & send data to /api/update-info

--- pages/dashbord.js
+++ pages/dashbord.js
@@ -1,10 +1,30 @@
 import { getSession } from 'next-auth/react';
-import React from 'react'
+import React, { useState } from 'react'
 
 const Dashbord = () => {
+  const [name , setName]= useState("");
+  const [lastName , setLastName]= useState("");
+  const [password , setPassword]= useState("");
+
+  const submitHandler=async ()=>{
+    const res = await fetch("/api/update-info" , {
+      method : "POST" ,
+      body : JSON.stringify({name , lastName , password}),
+      headers : {'Content-type': 'application/json'},
+    });
+
+    const data = await res.json();
+    console.log(data)
+  }
+
   return (
     <div>
-      Dashbord
+     <h1>Dashbord</h1>
+     <h3>Complete Your Profile</h3>
+     <input type='text' placeholder='Name' value={name} onChange={(e)=>  setName(e.target.value)} />
+     <input type='text' placeholder='Last Name' value={lastName} onChange={(e)=>  setLastName(e.target.value)} />
+     <input type='password' placeholder='Password' value={password} onChange={(e)=>  setPassword(e.target.value)} />
+     <button onClick={submitHandler}>Submit</button>
     </div>
   )
 }
